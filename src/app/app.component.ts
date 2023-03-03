4464b16a6f912ea6265f9e5caf29255f77519b70
unknown <kevinmcshane02@gmail.com>
2023-03-03 12:33:16 +0000

    lab5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-app';
+  count:number = 0;
+  hide:boolean = true;
+
+  increment() {
+    this.count++;
+  }
+  ondbl(){
+    if(this.hide === true)
+     this.hide = false;
+    else
+      this.hide = true;
+  }
 }
